efbd714e1989742bf321439b4448a26db85bca87
ERICHONG <f102041332@gmail.com>
2020-05-28 15:30:35 +0800

    php

--- gulpfile.js
+++ gulpfile.js
@@ -11,10 +11,14 @@ var imagemin = require('gulp-imagemin');
 var gutil = require( 'gulp-util' );
 var ftp = require( 'vinyl-ftp' );
 
+var connect = require('gulp-connect-php');
+
 // npm install --save-dev gulp-imagemin 壓圖工具
 
 // npm i gulp-util --save-dev  上傳ftp
 // npm i vinyl-ftp --save-dev  上傳ftp
+
+// npm install --save-dev gulp-connect-php  PHP
 
 // sass 轉譯
 gulp.task('sass', function () {
@@ -66,17 +70,39 @@ gulp.task( 'ftp',['minimage'],function () {
 
   var globs = [
       'dest/**',
-      'dest/css/**',
-      'dest/js/**',
-      'dest/images/**',
-      'index.html'
+      // 'dest/css/**',
+      // 'dest/js/**',
+      // 'dest/images/**',
+      // 'index.html',
   ];
 
   // using base = '.' will transfer everything to /public_html correctly
   // turn off buffering in gulp.src for best performance
 
   return gulp.src( globs, { base: '.', buffer: false } )
-      .pipe( conn.newer( './T2000256' ) ) // only upload newer files
-      .pipe( conn.dest( './T2000256' ) );
+      .pipe( conn.newer( './04.個人作品/web/T2000256/' ) ) // only upload newer files
+      .pipe( conn.dest( './04.個人作品/web/T2000256/' ) );
 
 } );
+
+// php
+gulp.task('php', function () {
+  connect.server({
+    base: './php',
+    port: 8010,
+    keepalive: true
+  });
+});
+gulp.task('browserSync',['php'], function() {
+  browserSync.init({
+      // proxy:"localhost:8010",
+      baseDir: "./php",
+      open:true,
+      notify:false
+
+  });
+});
+
+gulp.task('devphp', [ 'browserSync'], function() {
+  gulp.watch('./php/*.php', browserSync.reload);
+});
